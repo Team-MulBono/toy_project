3297df8783d750150f89bcc1cbc33af5570560c7
princeton <ssk0080@gmail.com>
2023-05-17 15:04:35 +0900

    feat: 소개 페이지 1(목차) 완성

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -1,5 +1,7 @@
 const welcomePage = document.querySelector(".welcome-page-area");
-const teamIntroducePage = document.querySelector(".team-introduce-area");
+const teamIntroduceListPage = document.querySelector(".page-one");
+const teamIntroducePage = document.querySelector(`.page-two`);
+
 
 
 if (!isIncludesHide(welcomePage)) {
@@ -8,12 +10,35 @@ if (!isIncludesHide(welcomePage)) {
 
     setTimeout(() => {
       welcomePage.classList.add("hide");
-      teamIntroducePage.classList.remove("hide");
-      teamIntroducePage.classList.add("animate__animated", "animate__heartBeat", "animate__slow")
+      teamIntroduceListPage.classList.remove("hide");
+      teamIntroduceListPage.classList.add("animate__animated", "animate__heartBeat", "animate__fast");
     }, 700);
   })
 }
 
 function isIncludesHide(document) {
   return document.className.includes("hide");
+}
+
+let isWheelEventActive = true;
+
+document.addEventListener("wheel", (e) => {
+  if (!isWheelEventActive) {
+    return;
+  }
+
+  console.log("scroll")
+
+  isWheelEventActive = false;
+  setTimeout(() => {
+    isWheelEventActive = true;
+  }, 1000);
+});
+
+function isWheelUp(e) {
+  return e.deltaY < 0;
+}
+
+function isWheelDown(e) {
+  return e.deltaY > 0;
 }
