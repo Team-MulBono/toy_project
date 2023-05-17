69174f0378963e567aa9434cb2340a26db9f12c9
9yuhyeon <kkh2742@gmail.com>
2023-05-18 02:06:53 +0900

    방명록 삭제 API 추가

--- app.py
+++ app.py
@@ -4,28 +4,33 @@ from bs4 import BeautifulSoup
 from flask import Flask, jsonify, request, render_template
 from pymongo import MongoClient
 import datetime as dt
-
+from bson import ObjectId
+from bson.json_util import dumps
 
 client = MongoClient('mongodb+srv://mulbono:1234@cluster0.fn6k9sb.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbmulbono
 
 app = Flask(__name__)
 
+
 # index 페이지 조회 API (index.html 랜더링 및 팀원 소개 data)
 @app.route('/index')
 def main():
     data = list(db.members.find({},{'_id':False}))
     json_data = json.dumps(data)
+
     return render_template('index.html', json_data=json_data)
 
 # 방명록 조회 API
 @app.route('/index/guest-book', methods=["GET"])
 def get_guest_book():
-    guest_books = list(db.guestbook.find({},{'_id':False}))
-    return jsonify({'response':guest_books})
+    guest_books = list(db.guestbook.find())
+    json_data = dumps(guest_books, default=str)
+    
+    return jsonify({'response':json_data})
 
 # 방명록 작성 API
-@app.route("/guestbook", methods=["POST"])
+@app.route("/index/guest-book", methods=["POST"])
 def guestbook_post():
     time = dt.datetime.now()
 
@@ -39,7 +44,17 @@ def guestbook_post():
         'time': time_receive,
     }
     db.guestbook.insert_one(doc)
+    
     return jsonify({'msg':'방명록 작성 완료!!'})
+
+# 방명록 삭제 API
+@app.route('/index/guest-book/<id>', methods=["POST"])
+def delete_guest_book(id):
+    db.guestbook.delete_one({'_id': ObjectId(id)})
+    
+    return jsonify({'msg':'삭제 완료!'})
+
+
 
 if __name__ == '__main__':  
     app.run('0.0.0.0',port=5000,debug=True)
